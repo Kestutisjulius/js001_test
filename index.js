703417c9442edb70bb0fa1f023a45a9a430eea9f
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-03-18 08:11:31 +0200

    most popular color names

--- index.js
+++ index.js
@@ -2,24 +2,87 @@ const people = [{
 	name: "James",
 	lastName: "Jack",
 	address: "H. Street 55",
-	color: "red",
+	color: "Red",
 	short: true
 }, {
 	name: "Pojo",
 	lastName: "Uncle",
 	address: "Opus. AV. d5",
-	color: "black",
+	color: "Black",
 	short: false
+},
+{
+	name: "Al",
+	lastName: "B",
+	address: "T. Street 2",
+	color: "Red",
+	short: true
+	},
+{
+	name: "Mary",
+	lastName: "M",
+	address: "M. Street 2m",
+	color: "Green",
+	short: false
+	},
+{
+	name: "Lou",
+	lastName: "L",
+	address: "L. Street 1L",
+	color: "Yellow",
+	short: false
+	},
+{
+	name: "Jack",
+	lastName: "Black",
+	address: "B. Street Jb",
+	color: "Yellow",
+	short: false
+	},
+{
+	name: "Jones",
+	lastName: "Jonson",
+	address: "J. Avenue 1F",
+	color: "Green",
+	short: false
+	},
+{
+	name: "Fi",
+	lastName: "Fo",
+	address: "Fu. AV. 3",
+	color: "Black",
+	short: true
 }
-
 	];
  const names = [];
- const colors = [];
+
+ 
+ //trufy, falsy -> ''-> false, undefined == false , 0 == false
+ /* atsirenkam kokiu spalvu ir kokiais kiekiais yra masyve */
+const colorsCount = {};
 for (const person of people) {
-	const {color} = person;
-	const {name} = person;
-	names.push(name);
-	colors.push(color);
+	const {color} = person; //destructuring ...
+	colorsCount[color] = colorsCount[color] ?  colorsCount[color] + 1 : 1;
 }
-console.log(colors);
-console.log(names);
+const colorNames = Object.keys(colorsCount);
+
+
+/* randame dazniausiai paminetos spalvos kieki (spalvos pavadinimas nera aktualus) */
+let mostPopularColorCount = 0;
+for ( const color in colorsCount ) {
+	const count = colorsCount[color];
+	if ( count > mostPopularColorCount) {
+		mostPopularColorCount = count;
+	}
+}
+
+/* randame visus spalvu pavadinimus, kurie atitinka didziausia paminejimu kieki */
+let mostPopularColorNames = [];
+for (const color in colorsCount) {
+	const count = colorsCount[color];
+	if ( count === mostPopularColorCount) {
+		mostPopularColorNames.push(color);
+	}
+}
+
+console.log(mostPopularColorNames);
